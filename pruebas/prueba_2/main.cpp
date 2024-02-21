785c114dadabc344a0a42eb7cfe0e175bdc400eb
pepesanyu00 <i82sayuj@uco.es>
2024-02-21 12:20:22 +0100

    debug

--- pruebas/prueba_2/main.cpp
+++ pruebas/prueba_2/main.cpp
@@ -27,7 +27,9 @@ int main() {
                 variable = 17;
                 COMMIT_STASK(tid,0,0);
             }
-
+        }
+        #pragma omp single
+        {
             #pragma omp task shared(variable)
             {
                 int tid = omp_get_thread_num();
