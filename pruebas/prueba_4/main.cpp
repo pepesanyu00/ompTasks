199665250f81973353d5bef48ca734d62a245c34
pepe <i82sayuj@uco.es>
2024-03-08 21:34:59 +0100

    debug

--- pruebas/prueba_4/main.cpp
+++ pruebas/prueba_4/main.cpp
@@ -31,17 +31,17 @@ int main(int argc, char *argv[]) {
             {
                 int tid = omp_get_thread_num();
                 //std::cout << "tid1:" << tid << endl;
-                BEGIN_STASK(tid,0,variable,0);
+                BEGIN_STASK(0,0,variable,0);
                 variable = 17;
-                COMMIT_STASK(tid,0,variable,0);
+                COMMIT_STASK(0,0,variable,0);
             }
             #pragma omp task shared(variable)
             {
                 int tid = omp_get_thread_num();
                 //std::cout << "tid2:" << tid << endl;
-                BEGIN_STASK(tid,1,0,variable);
+                BEGIN_STASK(1,0,0,variable);
                 variable2 = variable+1;
-                COMMIT_STASK(tid,1,0,variable);
+                COMMIT_STASK(1,0,0,variable);
             }
         }
     }
