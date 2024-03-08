074c1dda17535b1c6821450c6b01ca30e962bde7
pepesanyu00 <i82sayuj@uco.es>
2024-03-08 12:29:37 +0100

    debug

--- pruebas/prueba_4/main.cpp
+++ pruebas/prueba_4/main.cpp
@@ -23,6 +23,7 @@ int main(int argc, char *argv[]) {
       return 0;
     }
     tstart = chrono::steady_clock::now();
+    cout << "he pasado" << endl;
     #pragma omp parallel 
     {
         #pragma omp single
