1301adc369af653b453ef0e6e2dd84c77b5ed5e3
pepesanyu00 <i82sayuj@uco.es>
2024-03-12 09:31:40 +0100

    modificado script read_aborts

--- pruebas/prueba_4/read_aborts.sh
+++ pruebas/prueba_4/read_aborts.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # Directorio que contiene los archivos .stats
-directorio="stats"
+directorio="/ruta/del/directorio"
 
 # Iterar sobre cada archivo .stats en el directorio
 for archivo in "$directorio"/*.stats; do
     if [ -f "$archivo" ]; then
-        # Mostrar el contenido de la línea 9 del archivo actual
+        # Extraer la línea 9 del archivo actual y verificar si contiene "1"
         contenido_linea_9=$(sed -n '9p' "$archivo")
-        echo "Contenido de la línea 9 en $archivo: $contenido_linea_9"
+        if [[ $contenido_linea_9 == *"1"* ]]; then
+            echo "Contenido de la línea 9 en $archivo: $contenido_linea_9"
+        fi
     fi
 done
