7ea9057da0d075eaebde2074a2897df82e5a2e8f
pepesanyu00 <i82sayuj@uco.es>
2024-03-12 09:50:57 +0100

    debug

--- pruebas/prueba_4/read_aborts.sh
+++ pruebas/prueba_4/read_aborts.sh
@@ -8,7 +8,7 @@ for archivo in "$directorio"/*.stats; do
     if [ -f "$archivo" ]; then
         # Extraer la línea 9 del archivo actual y verificar si contiene más de un "1"
         contenido_linea_9=$(sed -n '9p' "$archivo")
-        num_ocurrencias=$(echo "$contenido_linea_9" | grep -o "2" | wc -l)
+        num_ocurrencias=$(echo "$contenido_linea_9" | grep -o "3" | wc -l)
         if [ "$num_ocurrencias" -gt 1 ]; then
             echo "Contenido de la línea 9 en $archivo: $contenido_linea_9"
         fi
