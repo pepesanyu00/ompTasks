e227d685f7a04e0ca6a31820451edce3ad5a9991
pepesanyu00 <i82sayuj@uco.es>
2024-03-12 10:41:23 +0100

    debug

--- pruebas/prueba_4/read_aborts.sh
+++ pruebas/prueba_4/read_aborts.sh
@@ -9,7 +9,7 @@ for archivo in "$directorio"/*.stats; do
         # Extraer la línea 9 del archivo actual y verificar si contiene un número mayor o igual que 1 más de 3 veces
         contenido_linea_9=$(sed -n '9p' "$archivo")
         num_ocurrencias=$(echo "$contenido_linea_9" | grep -o '[1-9]' | grep -c '[1-9]')
-        if [ "$num_ocurrencias" -ge 2 ]; then
+        if [ "$num_ocurrencias" -ge 3 ]; then
             echo "Contenido de la línea 9 en $archivo: $contenido_linea_9"
         fi
     fi
