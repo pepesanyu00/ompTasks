386f6a428196d5ec5a1c822d586553223bc62225
pepesanyu00 <i82sayuj@uco.es>
2024-03-12 09:42:33 +0100

    debug

--- pruebas/prueba_4/read_aborts.sh
+++ pruebas/prueba_4/read_aborts.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Directorio que contiene los archivos .stats
-directorio="stats"
+directorio="/ruta/del/directorio"
 
 # Iterar sobre cada archivo .stats en el directorio
 for archivo in "$directorio"/*.stats; do
     if [ -f "$archivo" ]; then
-        # Extraer la línea 9 del archivo actual y verificar si contiene "1"
-        contenido_linea_9=$(sed -n '9p' "$archivo" | grep "1")
-        if [ -n "$contenido_linea_9" ]; then
+        # Extraer la línea 9 del archivo actual y verificar si contiene más de un "1"
+        contenido_linea_9=$(sed -n '9p' "$archivo")
+        num_ocurrencias=$(echo "$contenido_linea_9" | grep -o "1" | wc -l)
+        if [ "$num_ocurrencias" -gt 1 ]; then
             echo "Contenido de la línea 9 en $archivo: $contenido_linea_9"
         fi
     fi
 done
-
