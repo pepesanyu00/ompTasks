a5a5b435ecc1c2a1a1b929abfca58212b99d0f3e
pepesanyu00 <i82sayuj@uco.es>
2024-03-12 10:39:27 +0100

    modificado read_aborts

--- pruebas/prueba_4/read_aborts.sh
+++ pruebas/prueba_4/read_aborts.sh
@@ -6,10 +6,10 @@ directorio="stats"
 # Iterar sobre cada archivo .stats en el directorio
 for archivo in "$directorio"/*.stats; do
     if [ -f "$archivo" ]; then
-        # Extraer la línea 9 del archivo actual y verificar si contiene más de un "1"
+        # Extraer la línea 9 del archivo actual y verificar si contiene un número mayor o igual que 1 más de 3 veces
         contenido_linea_9=$(sed -n '9p' "$archivo")
-        num_ocurrencias=$(echo "$contenido_linea_9" | grep -o "1" | wc -l)
-        if [ "$num_ocurrencias" -gt 1 ]; then
+        num_ocurrencias=$(echo "$contenido_linea_9" | grep -o '[1-9]' | grep -c '[1-9]')
+        if [ "$num_ocurrencias" -ge 3 ]; then
             echo "Contenido de la línea 9 en $archivo: $contenido_linea_9"
         fi
     fi
