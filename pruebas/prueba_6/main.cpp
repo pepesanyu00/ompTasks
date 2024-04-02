2281e7a904a339fa83ab6fff9efb4f3ae1d2d68c
pepe <i82sayuj@uco.es>
2024-04-02 13:48:03 +0200

    debug

--- pruebas/prueba_6/main.cpp
+++ pruebas/prueba_6/main.cpp
@@ -1,5 +1,4 @@
-/* Vamos a modificar un poco la prueba 5 para que las tareas se creen dentro del bucle anidado, estableciendo dependencias entre las variables
-que había anteriormente
+/* Esta prueba es una extensión de la prueba 4, 
 */
 
 
@@ -11,49 +10,83 @@ que había anteriormente
 
 using namespace std;
 
-void gauss_seidel(int tsteps, int size, int TS, int (*p)[size]) {
-    int NB = size / TS;
-    #pragma omp parallel
-    #pragma omp single
-        for (int t = 0; t < tsteps; ++t)
-            for (int ii=1; ii < size-1; ii+=TS)
-                for (int jj=1; jj < size-1; jj+=TS) {
-                    #pragma omp task depend(inout: p[ii:TS][jj:TS])
-                                    depend(in: p[ii-TS:TS][jj:TS], p[ii+TS:TS][jj:TS],
-                                    p[ii:TS][jj-TS:TS], p[ii:TS][jj:TS])
-                    {
-                    BEGIN_STASK(1, 0);
-                    for (int i=ii; i<(1+ii)*TS; ++i)
-                        for (int j=jj; j<(1+jj)*TS; ++j)
-                            p[i][j] = 0.25 * (p[i][j-1] * p[i][j+1] *
-                            p[i-1][j] * p[i+1][j]);
-                    }
-                    COMMIT_STASK(1, 0);
-                }
+#define SIZE 10
+
+void fill_matrix(int matrix[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; ++i) {
+        for (int j = 0; j < SIZE; ++j) {
+            matrix[i][j] = rand() % 100;  // Genera un número aleatorio entre 0 y 99
+        }
+    }
 }
 
-int main() {
-    const int tsteps = 10;
-    const int size = 100;
-    const int TS = 10;
-    int p[size][size];
+void print_matrix(int matrix[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; ++i) {
+        for (int j = 0; j < SIZE; ++j) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
 
-    // Inicializar la matriz p
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            p[i][j] = i * j;
+
+int main(int argc, char *argv[]) {
+
+    int a[SIZE][SIZE];
+    int b[SIZE][SIZE];
+    int c[SIZE][SIZE] = {};
+    int d[SIZE][SIZE] = {};
+
+    fill_matrix(a);
+    fill_matrix(b);
+
+    //print_matrix(a);
+    //print_matrix(b);
+    //print_matrix(c);
+
+    chrono::steady_clock::time_point tstart, tend;
+    chrono::duration<double> telapsed;
+    if(!statsFileInit(2)){
+      cout << "Error abriendo o inicializando el archivo de estadísticas." << endl;
+      return 0;
+    }
+    tstart = chrono::steady_clock::now();
+    #pragma omp parallel 
+    {
+        #pragma omp single
+        {
+            #pragma omp task shared(a, b, c)
+            {
+                BEGIN_STASK(c,0);
+                for (int i = 0; i < SIZE; ++i) {
+                    for (int j = 0; j < SIZE; ++j) {
+                        for (int k = 0; k < SIZE; ++k) {
+                            
+                            c[i][j] += a[i][k] * b[k][j];
+                        }
+                    }
+                }
+                COMMIT_STASK(c,0);
+            }
+            #pragma omp task shared(c)
+            {
+                BEGIN_STASK(0,c);
+                for (int i = 0; i < SIZE; ++i) {
+                    for (int j = 0; j < SIZE; ++j) {
+                        d[i][j] = c[i][j]+1;
+                    }
+                }
+                COMMIT_STASK(0,c);
+            }
         }
     }
-
-    gauss_seidel(tsteps, size, TS, p);
-
-    // Imprimir la matriz p después de gauss_seidel
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            cout << p[i][j] << " ";
-        }
-        cout << endl;
+    tend = chrono::steady_clock::now();    
+    telapsed = tend - tstart;
+    if(!dumpStats()){
+      cout << "Error volcando las estadísticas." << endl;
     }
-
+    cout << d[9][9] << endl;
+    cout << telapsed.count() << endl;
+    
     return 0;
 }
